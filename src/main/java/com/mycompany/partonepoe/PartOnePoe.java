ee23b4ed8bcadc797d11d1db7a68b018c5db11ed
kvmaq <kvmaq@KHALTURE>
2026-04-12 23:28:24 +0200

    Changed variable name of last name to txtLastName in the Edit Appointment frame

---
+++ src/main/java/com/mycompany/partonepoe/PartOnePoe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.partonepoe;
+
+/**
+ *
+ * @author kvmaq
+ */
+public class PartOnePoe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/partonepoe/User.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.partonepoe;
+
+/**
+ *
+ * @author kvmaq-Khanya Maqaqa
+ */
+public class User {
+    //Declaring Username,Password and Phonenumber variables
+    private String username;
+    private String password;
+    private String phonenumber;
+    
+    //Constructor to assign Global variables to Local variabless
+    public User(String username, String password, String phonenumber){  
+        this.username = username;
+        this.password = password;
+        this.phonenumber = phonenumber;
+    }
+            
+    public boolean checkUserName(){
+        return true;
+    }
+    
+    public boolean check
+    
+}
